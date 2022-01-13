f6be4f135b0c8359001d643e66a1e8deff059bd9
sergarb1 <sergi.profesor@gmail.com>
2022-01-13 18:10:58 +0100

    Añadidos histogramas

--- Google-HashCode/OnePizza2022/lanzar.sh
+++ Google-HashCode/OnePizza2022/lanzar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 chmod +x *.py
 
-./mainAleatorio.py < a_an_example.in > a_an_example.out
-./mainAleatorio.py < b_basic.in > b_basic.out
-./mainAleatorio.py < c_coarse.in > c_coarse.out
-./mainAleatorio.py < d_difficult.in > d_difficult.out
-./mainAleatorio.py < e_elaborate.in > e_elaborate.out
+./$1 < a_an_example.in > a_an_example.out
+./$1 < b_basic.in > b_basic.out
+./$1 < c_coarse.in > c_coarse.out
+./$1 < d_difficult.in > d_difficult.out
+./$1 < e_elaborate.in > e_elaborate.out
 
