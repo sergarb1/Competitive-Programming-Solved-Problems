10b36f1a6c1c61ccfa103b9d1bd546fef1512493
sergarb1 <sergi.profesor@gmail.com>
2022-01-13 15:18:46 +0100

    HashCode2022 Pizza

---
+++ Google-HashCode/OnePizza2022/lanzar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chmod +x *.py
+
+./mainAleatorio.py < a_an_example.in > a_an_example.out
+./mainAleatorio.py < b_basic.in > b_basic.out
+./mainAleatorio.py < c_coarse.in > c_coarse.out
+./mainAleatorio.py < d_difficult.in > d_difficult.out
+./mainAleatorio.py < e_elaborate.in > e_elaborate.out
+
---
+++ Google-HashCode/OnePizza2022/mainAleatorio.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import sys
+import math
+import random
+
+
+#Variables globales
+nClientes=0
+leGusta=[]
+noLeGusta=[]
+ingredientesDisponibles=set()
+
+
+#Imprimir solucion
+
+def imprimirSolucion(s):
+    cad=str(len(s))
+
+    for x in s:
+        cad=cad + " " + x
+
+    print(cad)
+
+#Main
+def main():
+    # Numero de potenciales clientes y otras variables globales
+    global nClientes, leGusta, noLeGusta,ingredientesDisponibles
+
+    #Leo el numero de potenciales clientes 
+    nClientes=int(input())
+
+
+
+
+    #Por cada cliente, leemos sus preferencias que gustan y no gustan
+    for i in range(nClientes):
+        leGusta.append(input().split(" ")[1:])
+        noLeGusta.append(input().split(" ")[1:])
+
+        #Incluimos ingredientes que gustan a la lista total de ingredientes
+        for ingr in leGusta[i]:
+            ingredientesDisponibles.add(ingr)
+
+    #Solucion aleatoria
+    sol= random.sample(ingredientesDisponibles,random.randint(1,len(ingredientesDisponibles)))
+    
+    imprimirSolucion(sol)
+
+
+# Codigo a ejecutar inicial
+main()
+
