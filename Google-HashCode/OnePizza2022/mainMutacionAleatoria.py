f80602b84a8807987754dcfae10be93ef1e2c50b
sergarb1 <sergi.profesor@gmail.com>
2022-02-15 20:14:22 +0100

    Nueva version

---
+++ Google-HashCode/OnePizza2022/mainMutacionAleatoria.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python3
+from os import remove
+import sys
+import math
+import random
+
+import numpy as np
+
+
+
+#Variables globales
+nClientes=0
+leGusta=[]
+noLeGusta=[]
+noLeGustaOrdenado=[]
+ingredientesDisponibles=set()
+listaTotal=[]
+profundidadIngredientesIni=0
+profundidadIngredientesFin=0
+maxNoGustaIngredientesIni=0
+maxNoGustaIngredientesFin=0
+
+#Imprimir solucion
+
+def imprimirSolucion(s):
+
+
+    global listaTotal
+    conjuntoSol=set()
+    for x in range(len(s)):
+        if(s[x]==1):
+            conjuntoSol.add(listaTotal[x])
+        
+    cad=""
+    for x in conjuntoSol:
+        cad=cad + " " + x
+
+    print(str(len(conjuntoSol))+cad)
+
+
+#Imprimir solucion
+
+def scoreSolucion(s):
+
+    # Numero de potenciales clientes y otras variables globales
+    global nClientes, leGusta, noLeGusta,noLeGustaOrdenado,ingredientesDisponibles ,listaTotal
+
+    conjuntoSol=set()
+    for x in range(len(s)):
+        if(s[x]==1):
+            conjuntoSol.add(listaTotal[x])
+        
+            
+    score=0
+    for x in range(nClientes):
+        # print(conjuntoSol)
+        # print(set(leGusta[x]))
+        # print(set(noLeGusta[x]))
+        if( len(conjuntoSol | set(leGusta[x]))==len(conjuntoSol) and len(conjuntoSol & set(noLeGusta[x]))==0):
+            score=score+1
+
+    return score
+
+
+
+def obtenerSolucion():
+
+    global listaTotal
+
+    f = open("D/1778.out", "r")
+    solucionBase=f.read().split(" ")[1:]
+    genes=[]
+    genesMejor=[]
+    score=1758
+
+    
+    for x in listaTotal:
+        if x in solucionBase:
+            genes.append(1)
+        else:
+            genes.append(0)
+
+    genesMejor=genes[:]
+
+
+    haCambiado=True
+    while(haCambiado):
+        genes=genesMejor[:]
+        haCambiado=False
+        for i in range(len(genes)):
+            genesJugar=genes[:]
+            if(genesJugar[i]==0):
+                genesJugar[i]=1
+            else:
+                genesJugar[i]=0
+            scoreTMP=scoreSolucion(genesJugar)
+
+            sys.stderr.write("Score: "+str(scoreTMP)+"\n")
+            if(scoreTMP>score):
+                score=scoreTMP
+                haCambiado=True
+                genesMejor=genesJugar[:]
+
+
+
+        genes=genesMejor[:]
+        for i in range(1000):
+            num=random.randint(0,len(genes)-1)
+            if(genes[num]==0):
+                genes[num]=1
+            else:
+                genes[num]=0
+            scoreTMP=scoreSolucion(genes)
+            sys.stderr.write("ScoreRandom: "+str(scoreTMP)+"\n")
+            if(scoreTMP>score):
+                score=scoreTMP
+                haCambiado=True
+                genesMejor=genes[:]
+            if(genes[num]==0):
+                genes[num]=1
+            else:
+                genes[num]=0
+
+
+    return genesMejor
+
+
+#Main
+def main():
+    # Numero de potenciales clientes y otras variables globales
+    global nClientes, leGusta, noLeGusta, noLeGustaOrdenado,ingredientesDisponibles,profundidadIngredientesIni,profundidadIngredientesFin, maxNoGustaIngredientesIni,maxNoGustaIngredientesFin, listaTotal
+
+
+    #Leo el numero de potenciales clientes 
+    nClientes=int(input())
+
+
+
+
+    #Por cada cliente, leemos sus preferencias que gustan y no gustan
+    for i in range(nClientes):
+        leGusta.append(input().split(" ")[1:])
+        noLeGusta.append(input().split(" ")[1:])
+
+        #Incluimos ingredientes que gustan a la lista total de ingredientes
+        for ingr in leGusta[i]:
+            ingredientesDisponibles.add(ingr)
+
+    #Solucion con todos los ingredientes
+    listaTotal= list(ingredientesDisponibles)
+
+    #Ingredientes a eliminar
+    #Profundidad ingredientes
+    sol=obtenerSolucion()
+
+
+
+
+    imprimirSolucion(sol)
+    #Imprimimos score en stderr
+    sys.stderr.write("Score: "+str(scoreSolucion(sol))+"\n")
+
+# Codigo a ejecutar inicial
+main()
+
